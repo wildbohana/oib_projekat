b7e060533ba14408e05ad620bf7fd6a00f233f79
Михајловић Бојана <83251325+wildbohana@users.noreply.github.com>
2023-10-18 12:28:12 +0200

    Samo komponente, bez WCF
    WCF dodajem kasnije

---
+++ Projekat/Common/IServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+// Samo za serverske komponente se pravi interfejs
+// Klijenti su klijenti, ne treba ništa
+
+namespace Common
+{
+    [ServiceContract]
+    public interface IServer
+    {
+        // Mock funkcija
+        [OperationContract]
+        void Autentifikuj();
+
+        [OperationContract]
+        void Autorizuj();
+    }
+}
